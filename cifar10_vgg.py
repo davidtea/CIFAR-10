bfbe9f0e452d458191528576a6df93a2b39da5e7
David Tea <david.tea0824@gmail.com>
2017-03-26 18:23:39 -0700

    Updated vgg

--- cifar10_vgg.py
+++ cifar10_vgg.py
@@ -23,7 +23,7 @@ now = time.time()
 
 batch_size = 32
 num_classes = 10
-epochs = 25
+epochs = 2
 data_augmentation = True
 
 # input image dimensions
@@ -46,52 +46,52 @@ model = Sequential()
 # Block 1
 model.add(Conv2D(64, (3, 3), padding='same',input_shape=x_train.shape[1:]))
 model.add(Activation('relu'))
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Block 2
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(128, (3, 3)))
 model.add(Activation('relu'))
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(128, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Block 3
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(256, (3, 3)))
 model.add(Activation('relu'))
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(256, (3, 3)))
 model.add(Activation('relu'))
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(256, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Block 4
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(512, (3, 3)))
 model.add(Activation('relu'))
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(512, (3, 3)))
 model.add(Activation('relu'))
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(512, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Block 5
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(512, (3, 3)))
 model.add(Activation('relu'))
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(512, (3, 3)))
 model.add(Activation('relu'))
-#model.add(ZeroPadding2D((1,1)))
+model.add(ZeroPadding2D((1,1)))
 model.add(Conv2D(512, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -108,7 +108,7 @@ model.add(Activation('softmax'))
 
 sgd = SGD(lr=1.0, decay=1e-5, momentum=0.9, nesterov=True)
 adam = keras.optimizers.Adam(lr=1)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy', 'loss'])
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 # Normalize
 x_train = x_train.astype('float32')
